b2bf9980fed15c5cb8828be06ac94db58ade9ffa
lalatina11 <rahmadancandra@gmail.com>
2025-12-07 21:09:18 +0700

    chore: add date at index route

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,10 @@ import { Hono } from "hono";
 const app = new Hono();
 
 app.get("/", async (c) => {
-  return c.json({ message: "Hono api created by Candra" }, 200);
+  return c.json(
+    { message: "Hono api created by Candra", date: new Date(Date.now()) },
+    200,
+  );
 });
 
 export default app;
